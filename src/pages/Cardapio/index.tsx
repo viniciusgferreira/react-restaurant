9d937733a376c1b7a63888d5c47682355a0b5c9c
ViniciusGFerreira <vinicius.gf@outlook.com>
2023-05-13 21:08:21 -0300

    follow up tutorial

---
+++ src/pages/Cardapio/index.tsx
@@ -0,0 +1,27 @@
+import { useState } from "react";
+import Buscador from "./Buscador";
+import styles from "./Cardapio.module.scss";
+import { ReactComponent as Logo } from 'assets/logo.svg'
+import Filtros from "./Filtros";
+
+export default function Cardapio() {
+    const [busca, setBusca] = useState('');
+    const [filtro, setFiltro] = useState<number | null>(null)
+    return (
+        <main>
+            <nav className={styles.menu}>
+                <Logo />
+            </nav>
+            <header className={styles.header}>
+                <div className={styles.header__text}>A casa da Massa </div>
+            </header>
+            <section className={styles.cardapio}>
+                <h3 className={styles.cardapio__titulo}>Cardápio</h3>
+                <Buscador busca={busca} setBusca={setBusca} />
+                <div className={styles.cardapio__filtros}>
+                    <Filtros filtro={filtro} setFiltro={setFiltro} />
+                </div>
+            </section>
+        </main>
+    )
+}
